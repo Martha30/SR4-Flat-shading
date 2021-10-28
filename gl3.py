d40d8a1717b86fd07badc061d3d52d7e8fa2c2fc
Martha30 <53914325+Martha30@users.noreply.github.com>
2021-10-27 18:37:38 -0600

    últimos cambios

--- gl3.py
+++ gl3.py
@@ -241,6 +241,21 @@ class Renderer(object):
             for y in range(self.height):
                 for x in range(self.width):
                     file.write(self.pixels[x][y])
+    
+     def glFill(self, polygon):
+        for y in range(self.height):
+            for x in range(self.width):
+                i = 0
+                j = len(polygon) - 1
+                d = False
+                
+                for i in range(len(polygon)):
+                    if (polygon[i][1] < y and polygon[j][1] >= y) or (polygon[j][1] < y and polygon[i][1] >= y):
+                        if polygon[i][0] + (y - polygon[i][1]) / (polygon[j][1] - polygon[i][1]) * (polygon[j][0] - polygon[i][0]) < x:
+                            d = not d
+                    j = i
+                if d:
+                    self.glPoint((float(x)/(float(self.width)/2))-1,(float(y)/(float(self.height)/2))-1,self.vertex_color)
 
 
     def triangle(self):
